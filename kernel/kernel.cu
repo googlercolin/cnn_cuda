a65e49c5086c2124ebce61bc38a0beb9e05fc270
Colin Hong <colin.hong18@gmail.com>
2023-03-09 07:03:28 -0500

    changed type of sum variable in kernel.cu

--- kernel/kernel.cu
+++ kernel/kernel.cu
@@ -23,7 +23,7 @@ extern "C" __global__ void convolution_layer(double input[100][100],
     int input_x_idx = x*5;
     int input_y_idx = y*5;
 
-    int sum = 0;
+    double sum = 0;
 
     for(int i = 0; i < 5; i++) {
         for(int j = 0; j < 5; j++) {
